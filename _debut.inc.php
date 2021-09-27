53928ac16d5240611f9acf562c82cc95a30ea5fc
comoco92 <raouf.abdoumsa@hotmail.com>
2021-09-27 14:47:45 +0200

    ajout de la BD

--- _debut.inc.php
+++ _debut.inc.php
@@ -1,6 +1,6 @@
-<?
+<?php
 
-echo '
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
 "http://www.w3.org/TR/html4/loose.dtd">
 <!-- TITRE ET MENUS -->
@@ -34,5 +34,7 @@ echo '
    </tr>
 </table>
 <br>';
+
+?>
 
 
